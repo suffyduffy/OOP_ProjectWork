dfad0c2714541ea23cb3609780974391b6e76e34
efw234 <152409352+efw234@users.noreply.github.com>
2024-03-12 23:47:45 +0800

    Person movement and collision logic implemented. Small bug where he also appears on the start game button scene.

--- desktop/src/com/mygdx/game_engine/Managers/DesktopLauncher.java
+++ desktop/src/com/mygdx/game_engine/Managers/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		// Set up LWJGL3 configurations
-		config.setTitle("FatFuck");
+		config.setTitle("HealthyEating");
 		config.setWindowedMode(800, 600); // Set your desired width and height
 		new Lwjgl3Application(new GameManager(), config);
 	}
--- desktop/src/com/mygdx/game_engine/Managers/GameManager.java
+++ desktop/src/com/mygdx/game_engine/Managers/GameManager.java
@@ -2,8 +2,11 @@ package com.mygdx.game_engine.Managers;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game_layer.Objects.Entities;
 import com.mygdx.game_layer.Objects.HealthyFoodItem;
 import com.mygdx.game_layer.Objects.TexturedObject;
@@ -32,6 +35,14 @@ public class GameManager extends Game {
     private int nextEntityIndex = 0;
     private List<Entities> allEntities = new ArrayList<>();
 
+    // Additional image at the bottom
+    private Texture fitManTexture;
+    private Texture skinnyManTexture;
+    private Texture fatManTexture;
+    private Vector2 playerPosition;
+    private boolean isSkinny = false;
+    private boolean isFat = false;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
@@ -46,6 +57,14 @@ public class GameManager extends Game {
         sceneManager.addScene(menuScene);
         sceneManager.setCurrentScene(menuScene);
 
+        // Load additional images
+        fitManTexture = new Texture("Sprites/fitman.png");
+        skinnyManTexture = new Texture("Sprites/skinnyman.png");
+        fatManTexture = new Texture("Sprites/fatman.png");
+        playerPosition = new Vector2(
+                (Gdx.graphics.getWidth() - fitManTexture.getWidth()) / 2,
+                0
+        );
 
         // Prepare all entities
         prepareEntities();
@@ -80,7 +99,6 @@ public class GameManager extends Game {
         // Add more entities as needed
     }
 
-
     @Override
     public void render() {
         super.render();
@@ -91,8 +109,46 @@ public class GameManager extends Game {
         Scene currentScene = sceneManager.getCurrentScene();
         if (currentScene != null) {
             currentScene.render(batch);
+
+            // Draw player
+            Texture playerTexture = fitManTexture;
+            if (isSkinny) {
+                playerTexture = skinnyManTexture;
+            } else if (isFat) {
+                playerTexture = fatManTexture;
+            }
+            batch.draw(playerTexture, playerPosition.x, playerPosition.y);
+
         }
         batch.end();
+    }
+
+    private void handlePlayerMovement(float deltaTime) {
+        float playerSpeed = isSkinny ? 300f : (isFat ? 100f : 200f); // Adjust speed based on player state
+
+        // Move player left
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            playerPosition.x -= playerSpeed * deltaTime;
+            // Ensure player does not go beyond left screen border
+            playerPosition.x = Math.max(0, playerPosition.x);
+        }
+        // Move player right
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            float playerWidth = getPlayerWidth();
+            playerPosition.x += playerSpeed * deltaTime;
+            // Ensure player does not go beyond right screen border
+            playerPosition.x = Math.min(Gdx.graphics.getWidth() - playerWidth, playerPosition.x);
+        }
+    }
+
+    private float getPlayerWidth() {
+        Texture playerTexture = fitManTexture;
+        if (isSkinny) {
+            playerTexture = skinnyManTexture;
+        } else if (isFat) {
+            playerTexture = fatManTexture;
+        }
+        return playerTexture.getWidth();
     }
 
     private void update(float deltaTime) {
@@ -110,6 +166,8 @@ public class GameManager extends Game {
             }
         }
 
+        handlePlayerMovement(deltaTime);
+
         // Update positions of food items
         for (Entities entity : entityManager.getEntitiesList()) {
             for (TexturedObject texturedObject : entity.getTexturedObjects()) {
@@ -126,10 +184,42 @@ public class GameManager extends Game {
                 if (texturedObject.getPosition().y < 0) {
                     texturedObject.getPosition().y = Gdx.graphics.getHeight();
                 }
+
+                // Check for collision with player
+                if (checkCollision(texturedObject)) {
+                    if (entity instanceof HealthyFoodItem) {
+                        isSkinny = true;
+                        isFat = false;
+                    } else if (entity instanceof UnhealthyFoodItem) {
+                        isSkinny = false;
+                        isFat = true;
+                    }
+                    entityManager.removeEntity(entity);
+                }
             }
         }
     }
 
+    private boolean checkCollision(TexturedObject foodItem) {
+        float playerWidth = getPlayerWidth();
+        float playerHeight = fitManTexture.getHeight(); // Assume all player textures have the same height
+
+        // Calculate player bounding box
+        float playerLeft = playerPosition.x;
+        float playerRight = playerPosition.x + playerWidth;
+        float playerTop = playerPosition.y + playerHeight;
+        float playerBottom = playerPosition.y;
+
+        // Calculate food item bounding box
+        float foodLeft = foodItem.getPosition().x;
+        float foodRight = foodItem.getPosition().x + foodItem.getTexture().getWidth();
+        float foodTop = foodItem.getPosition().y + foodItem.getTexture().getHeight();
+        float foodBottom = foodItem.getPosition().y;
+
+        // Check for collision
+        return playerRight >= foodLeft && playerLeft <= foodRight &&
+                playerBottom <= foodTop && playerTop >= foodBottom;
+    }
 
     @Override
     public void dispose() {
@@ -145,6 +235,10 @@ public class GameManager extends Game {
         if (music != null) {
             music.dispose();
         }
+        // Dispose additional image textures
+        fitManTexture.dispose();
+        skinnyManTexture.dispose();
+        fatManTexture.dispose();
     }
 
     public SceneManager getSceneManager() {
